93d5b631c881ab3e4622ec4fa7817c214427a1a6
habr <jonas.habr@t-mobile.cz>
2019-11-15 12:10:35 +0100

    Updates

---
+++ precache-manifest.96a4bec2503bef9ed28c80445003abaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca934df9f7d530e8b758778db1b6fe3",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "b38a28f44b9975dfae81",
+    "url": "/todos/static/css/main.4c79d0d2.chunk.css"
+  },
+  {
+    "revision": "2a013bca7b5a062cbc3b",
+    "url": "/todos/static/js/2.6cbc83bc.chunk.js"
+  },
+  {
+    "revision": "b38a28f44b9975dfae81",
+    "url": "/todos/static/js/main.b5ad90c5.chunk.js"
+  },
+  {
+    "revision": "53ddf9a525b55b103aa1",
+    "url": "/todos/static/js/runtime-main.8acf62dc.js"
+  }
+]);
